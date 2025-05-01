697bb68cd16b5e08a8943ce6d6ae8fa88be05f19
jayveepcrujido <jvcrujido@gmail.com>
2025-05-01 12:20:15 +0800

    final commit

--- inventory_manager.py
+++ inventory_manager.py
@@ -31,6 +31,7 @@ class FruitInventoryManager:
         return data.inventory.get(item, 0)
 
 
+# CLI
 def main():
     manager = FruitInventoryManager()
 
@@ -39,6 +40,8 @@ def main():
         print("1. Add Item")
         print("2. Remove Item")
         print("3. View Inventory")
+        print("4. Check Stock")
+        print("5. Exit")
 
         try:
             choice = input("Choose an option: ")
@@ -82,6 +85,21 @@ def main():
                     for item, qty in inventory.items():
                         if qty > 0:
                             print(f"{qty} of {item}(s)")
+
+            elif choice == "4":
+                item = input("Enter item name: ").lower()
+                stock = manager.get_stock(item)
+                if stock == 0:
+                    print(f"\n{item} not found")
+                else:
+                    print(f"\nStock of {item}: {stock}")
+
+            elif choice == "5":
+                print("\nExiting Fruit Inventory Manager.")
+                break
+
+            else:
+                print("\nInvalid choice.")
 
         except ValueError as ve:
             print(f"\nInput error: {ve}")
